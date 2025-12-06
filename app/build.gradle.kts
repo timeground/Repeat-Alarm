34565b962384ffd2e36bbcb4c0501e7ad2931418
timeground <athul@timeground.in>
2025-12-06 22:17:18 +0530

    Remove sw600dp dimensions to fix zoomed-in tablet UI, bump v3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.timeground.repeatreminder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "3.7"
+        versionCode = 29
+        versionName = "3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
